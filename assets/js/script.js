93d5147191ce2f396a93d39aefbf66ef6685a7b5
pswhdev <patriciaswhalley@gmail.com>
2024-02-25 13:53:33 +0100

    Add favicon and final tidy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,12 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   }
+
+  document.getElementById('answer-box').addEventListener('keydown', function(event){
+if (event.key === 'Enter') {
+    checkAnswer();
+}
+  })
   runGame("addition");
 });
 
@@ -21,6 +27,8 @@ document.addEventListener("DOMContentLoaded", function () {
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
   // creates two random numbers between 1  and 25
   let num1 = Math.floor(Math.random() * 25) + 1;
   let num2 = Math.floor(Math.random() * 25) + 1;
@@ -104,8 +112,10 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractionQuestion(operand1, operand2) {
-  document.getElementById("operand1").textContent = operand1;
-  document.getElementById("operand2").textContent = operand2;
+  document.getElementById("operand1").textContent =
+    operand1 > operand2 ? operand1 : operand2;
+  document.getElementById("operand2").textContent =
+    operand1 > operand2 ? operand2 : operand1;
   document.getElementById("operator").textContent = "-";
 }
 
